43a1b9de2743e0769b4e9a9e620364de51f3825b
lee <a31257122@gmail.com>
2023-08-16 16:36:43 +0900

    20230816

--- lib/main.dart
+++ lib/main.dart
@@ -13,70 +13,74 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  TextEditingController textEditingController = TextEditingController();
-  var enabled = false;
+  var controller = TextEditingController();
+  bool enabled = false;
   List<Score> score = [];
   double rate = 0;
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
-    score.add(Score(rate: 2.5, comment: '맛나요'));
+    score.add(Score(rate: 5, comment: '맛나요'));
+    score.add(Score(rate: 1, comment: '카레맛 똥'));
   }
 
   @override
   Widget build(BuildContext context) {
     var listView = ListView.builder(
       itemBuilder: (context, index) => ListTile(
-        leading: Text('${score[index].rate}'),
-        title: Text(score[index].comment),
-      ),
+          leading: Text('${score[index].rate}'),
+          title: Text(score[index].comment)),
     );
     return MaterialApp(
       home: Scaffold(
-          body: Column(
-        children: [
-          RatingBar.builder(
-            initialRating: 3,
-            minRating: 1,
-            direction: Axis.horizontal,
-            allowHalfRating: true,
-            itemCount: 5,
-            itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-            itemBuilder: (context, _) => const Icon(
-              Icons.star,
-              color: Colors.amber,
+        body: Column(
+          children: [
+            RatingBar.builder(
+              initialRating: 3,
+              minRating: 1,
+              direction: Axis.horizontal,
+              allowHalfRating: true,
+              itemCount: 5,
+              itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+              itemBuilder: (context, _) => const Icon(
+                Icons.star,
+                color: Colors.yellow,
+              ),
+              onRatingUpdate: (rating) {
+                setState(() {
+                  rate = rating;
+                  enabled = true;
+                });
+              },
             ),
-            onRatingUpdate: (value) {
-              setState(() {
-                enabled = true;
-                rate = value;
-              });
-            },
-          ),
-          TextFormField(
-            controller: textEditingController,
-            enabled: enabled,
-            decoration: const InputDecoration(
-                hintText: '한마디 해주세요.', label: Text('여긴뭘까')),
-            maxLength: 200,
-          ),
-          ElevatedButton(
-            onPressed: enabled
-                ? () {
-                    score.add(
-                        Score(rate: rate, comment: textEditingController.text));
-                    setState(() {
-                      listView;
-                    });
-                  }
-                : null,
-            child: const Text('저장하기'),
-          ),
-          Expanded(
-            child: listView,
-          ),
-        ],
-      )),
+            TextFormField(
+              controller: controller,
+              enabled: enabled,
+              decoration: const InputDecoration(
+                hintText: '한마디 해주세요',
+                label: Text('한마디'),
+                border: OutlineInputBorder(),
+              ),
+              maxLength: 30,
+            ),
+            ElevatedButton(
+              onPressed: enabled
+                  ? () {
+                      score.add(
+                        Score(rate: rate, comment: controller.text),
+                      );
+                      setState(() {
+                        ListView;
+                      });
+                    }
+                  : null,
+              child: const Text('저장하기'),
+            ),
+            Expanded(child: listView)
+          ],
+        ),
+      ),
     );
   }
 }
